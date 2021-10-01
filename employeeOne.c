533486dff03fde249929433b38b5c65b19f15351
norivera <89235780+norivera@users.noreply.github.com>
2021-09-30 22:39:50 -0700

    completed salary and phone functions

--- employeeOne.c
+++ employeeOne.c
@@ -27,3 +27,31 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
     }
     return NULL;
 }
+
+//Function to search for employee by phone
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tablesize, char * targetPhone)
+{
+    const PtrToConstEmployee endPtr = ptr + tablesize;
+    for(; ptr < endPtr; ptr++)
+    {
+        if(strcmp(ptr->phone,targetPhone)==0)
+        {
+            return (PtrToEmployee) ptr;
+        }
+    }
+    return NULL;
+}
+
+//function to seach employee by salary
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tablesize, double targetSalary)
+{
+    const PtrToConstEmployee endPtr = ptr + tablesize;
+    for(; ptr < endPtr; ptr++)
+    {
+        if(ptr->salary == targetSalary)
+        {
+            return (PtrToEmployee) ptr;
+        }
+    }
+    return NULL;
+}
